e5fb7c9fd5d88b8d08096c097503e2b06bb763ee
Louis Fernando <ferlouis55@gmail.com>
2024-08-22 17:25:52 +0700

    FINAL VERSION 1.6

--- resources/views/admin/shipmentLabel.blade.php
+++ resources/views/admin/shipmentLabel.blade.php
@@ -10,65 +10,83 @@
 </head>
 
 <body style="font-family: 'Montserrat', sans-serif; margin: 0; padding: 0;" width="100%">
-    <div style="width: 100%; height: 100%; padding: 1rem; padding-right: 0rem;">
+    <div style="width: 100%; height: 100%; padding: 1rem;">
         <div style="text-align: center; margin-bottom: 2mm;">
-            <h1 style="margin: 0; font-size: 14pt;">Label Pengiriman</h1>
+            <h1 style="margin: 0; font-size: 10pt;">Label Pengiriman</h1>
         </div>
         <div style="margin-bottom: 2mm;">
-            <table style="width: 100%; border-collapse: collapse;">
-                <tr>
-                    <td>
-                        <div>
-                            <p style="margin: 0; font-size:10pt;"><strong>Penerima:</strong></p>
-                            <p style="margin: 0; font-size:10pt;">{{ $order->user->name }}</p>
-                            <p style="margin: 0; font-size:10pt;">{{ $order->user->phone_number }}</p>
-                            <p style="margin: 0; font-size:10pt;">{{ $order->address->address }},
+            <table style="width: 100%; border-collapse: collapse; border: 1px solid black;">
+                <thead>
+                    <tr style="border: 1px solid black;">
+                        <th style="border: 1px solid black; font-size: 8pt; text-align: center;">Penerima</th>
+                        <th style="border: 1px solid black; font-size: 8pt; text-align: center;">Pengirim</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr style="border: 1px solid black;">
+                        <td style="width: 50%; border: 1px solid black; text-align: left;">
+                            <p style="margin: 0; font-size:8pt;">{{ $order->user->name }}</p>
+                            <p style="margin: 0; font-size:8pt;">{{ $order->user->phone_number }}</p>
+                            <p style="margin: 0; font-size:8pt;">{{ $order->address->address }},
                                 {{ $order->address->city }}, {{ $order->address->province }},
                                 {{ $order->address->postal_code }}</p>
-                        </div>
-                    </td>
-                    <td>
-                        <div>
-                            <p style="margin: 0; font-size:10pt;"><strong>Pengirim:</strong></p>
-                            <p style="margin: 0; font-size:10pt;">Lisahwan</p>
-                            <p style="margin: 0; font-size:10pt;">082230308030</p>
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <p style="margin: 0; font-size:10pt;"><strong>Kurir:</strong></p>
-                        <p style="margin: 0; font-size:10pt;">{{ $order->shipment_service }} ({{ $order->total_weight }} gram)</p>
-                    </td>
-                </tr>
+                        </td>
+                        <td style="width: 50%; border: 1px solid black; text-align: left;">
+                            <p style="margin: 0; font-size:8pt;">Lisahwan</p>
+                            <p style="margin: 0; font-size:8pt;">082230308030</p>
+                        </td>
+                    </tr>
+                    <tr style="border: 1px solid black;">
+                        <th style="border: 1px solid black; font-size: 8pt; text-align: center;" colspan="2">Kurir
+                        </th>
+                    </tr>
+                    <tr style="border: 1px solid black;">
+                        <td style="border: 1px solid black; text-align: left;" colspan="2">
+                            <p style="margin: 0; font-size:8pt;">{{ $order->shipment_service }}
+                                ({{ $order->total_weight }} gram)</p>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </div>
         <div style="margin-bottom: 0mm;">
-            <p style="margin: 0; margin-bottom: 1mm; font-size: 8pt;"><strong>Detail Pesanan:</strong></p>
             <table style="width: 100%; border-collapse: collapse; font-size: 8pt; border: 1px solid #000;">
                 <thead>
                     <tr>
-                        <th style="border: 1px solid #000; padding: 0.5mm; text-align: left;" width="50%">Produk</th>
-                        <th style="border: 1px solid #000; padding: 0.5mm; text-align: left;" width="50%">Jumlah</th>
+                        <th style="border: 1px solid #000; padding: 0.5mm; text-align: center;" colspan="2">Detail
+                            Pesanan</th>
+                    </tr>
+                    <tr>
+                        <th style="border: 1px solid #000; padding: 0.5mm; text-align: center;" width="50%">Produk
+                        </th>
+                        <th style="border: 1px solid #000; padding: 0.5mm; text-align: center;" width="50%">Jumlah
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($order->order_detail as $order_detail)
                         <tr>
-                            <td style="border: 1px solid #000; padding: 0.5mm;">{{ $order_detail->product->name }}</td>
-                            <td style="border: 1px solid #000; padding: 0.5mm;">{{ $order_detail->quantity }}</td>
+                            <td style="border: 1px solid #000; padding: 0.5mm; text-align: left;">
+                                {{ $order_detail->product->name }}</td>
+                            <td style="border: 1px solid #000; padding: 0.5mm; text-align: left;">
+                                {{ $order_detail->quantity }}</td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
             <p style="margin: 0; margin-top: 1mm; font-size: 8pt;"><strong>Total Produk:
                     {{ $order->order_detail->sum('quantity') }}</strong></p>
+            @if ($order->note)
+                <p style="margin: 0; font-size: 8pt; font-weight: bold; font-style: italic;">
+                    *{{ $order->note }}
+                </p>
+            @endif
         </div>
-        <div style="text-align: center; border-top: 1px solid #000; padding-top: 2mm; margin-top: 4mm;">
-            {{-- <p style="margin: 0; margin-bottom: 1mm; font-size: 10pt; text-align: center;"><strong>ISI MUDAH PECAH,
-                    JANGAN DIBANTING!</strong></p> --}}
+        <div style="text-align: center; border-top: 1px solid #000; padding-top: 2mm; margin-top: 3mm;">
+            <p style="margin: 0; font-size: 8pt; text-align: center;"><strong>Terima kasih sudah
+                    belanja!</strong></p>
             <img src="{{ url('/images/lisahwan_text.png') }}" alt="Lisahwan"
-                style="filter: contrast(150%) drop-shadow(2px 2px 2px black);" width="110px">
+                style="filter: contrast(150%) drop-shadow(2px 2px 2px black);" width="100px">
         </div>
     </div>
 </body>
